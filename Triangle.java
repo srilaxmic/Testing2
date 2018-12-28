c71417974b360d1e40629a34d1a48e7e7c5d2039
srilaxmic <csrilaxmi154@gmail.com>
2018-12-28 10:31:18 +0530

    reinitialized files

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+package com.geekbull.maven.Testing2;
+
+public class Triangle {
+
+		double a,b,h;
+		
+		public void area()
+		{
+			
+		a=0.5*b*h;
+		
+	System.out.println("area of triangle:" + a);
+	
+
+		}
+
+}
---
+++ Triangle1.java
@@ -0,0 +1,13 @@
+package com.geekbull.maven.Testing2;
+
+public class Triangle1 {
+
+	public static void main(String[] args) {
+		Triangle t=new Triangle();
+		t.b=3;
+		t.h=4;
+		t.area();
+
+	}
+
+}
